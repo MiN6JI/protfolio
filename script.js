292dc19756fb6737981bfaac61cf5793645c4e99
MINGJI <deorukhkarshruti232@gmail.com>
2024-07-16 20:51:44 +0530

    Changes

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const faqs = document.querySelectorAll(".faq-block");
 
-faqs.forEach((faq) =>{
-    faq.addEventListener("click", ()=>{
-        faq.classList.toggle("active")
-    });
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
 });
+
+let menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "100vh";
+    menuList.style.height = "100vh";
+
+    document.body.classList.add("no-scroll");
+  } else {
+    menuList.style.maxHeight = "0px";
+    document.body.classList.remove("no-scroll");
+  }
+}
